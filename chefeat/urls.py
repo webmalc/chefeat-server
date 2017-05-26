8eb0c3d86d2b764b2134405a5824ebed84b3adcc
webmalc <m@webmalc.pw>
2017-05-26 22:10:37 +0300

    rename base app

--- chefeat/urls.py
+++ chefeat/urls.py
@@ -1,4 +1,4 @@
-"""cheafeat URL Configuration
+"""chefeat URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
--- chefeat/wsgi.py
+++ chefeat/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cheafeat project.
+WSGI config for chefeat project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cheafeat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chefeat.settings")
 
 application = get_wsgi_application()
